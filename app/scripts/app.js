ce5083a01ae15e37e515e68b10e6562ae1c1733e
Philip Joubert <philipjoubert@Philips-iMac.local>
2015-06-28 12:28:50 -0700

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/* global app:true */
+/* exported app */
+
+/**
+ * @ngdoc overview
+ * @name angNewsApp
+ * @description
+ * # angNewsApp
+ *
+ * Main module of the application.
+ */
+var app = angular
+  .module('angNewsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl'
+      })
+      .when('/test', {
+        templateUrl: 'views/test.html',
+        controller: 'TestCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/test.js
@@ -0,0 +1,5 @@
+'use strict';
+
+app.controller("TestCtrl", function($scope) {
+	$scope.name = "Lolcat";
+});
